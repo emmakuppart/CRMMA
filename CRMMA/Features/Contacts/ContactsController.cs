2879224ad91b1b06c3a8ae21eaf59524d85aa706
Emma Kuppart <25949098+emmakuppart@users.noreply.github.com>
2019-01-30 13:24:49 +0200

    Add project files

---
+++ CRMMA/Features/Contacts/ContactsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CRMMAEngine.Features
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly CrmContext _context;
+
+        public ContactsController(CrmContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<List<ContactDto>> Get()
+        {
+            return await _context.Contacts.ToListAsync();
+        }
+    }
+}
---
+++ CRMMA/Features/CrmContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRMMAEngine.Features
+{
+    public class CrmContext : DbContext
+    {
+        public CrmContext(DbContextOptions<CrmContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContactDto> Contacts { get; set; }
+    }
+}
